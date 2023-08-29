1e3807cd0cc8f0e807d8060c7e1e201152747637
tg <T.Gautier@gys.fr>
2023-08-29 17:11:36 +0200

    Passage en multi-fichiers

--- main.py
+++ main.py
@@ -6,18 +6,24 @@ Programme transformant une image en matrice de nombres entre 0 et 1
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import os
 
-# Importation de l'image
-image = mpimg.imread("data/image.png")
+# Liste des images à traiter (dans le dossier data/to_convert)
+images_name = os.listdir("data/to_convert")
+for img_name in images_name:
+    # Importation de l'image
+    image = mpimg.imread("data/to_convert/" + img_name)
 
-# Conversion de l'image en matrice de nombres entre 0 et 1
-image = np.array(image[:, :, 0])
-image = 1 - image
-image = image / np.max(image)
+    # Conversion de l'image en matrice de nombres entre 0 et 1
+    image = np.array(image[:, :, 0])
+    image = 1 - image
+    image = image / np.max(image)
 
-# Affichage de l'image
-plt.imshow(image, cmap="gray")
-plt.show()
+    # Affichage de l'image
+    # plt.imshow(image, cmap="gray")
+    # plt.show()
 
-# Exportation de l'image
-np.savetxt("data/image.txt", image)
+    # Exportation de l'image
+    np.savetxt("data/done/" + img_name, image)
+
+    print(img_name + " done !")
